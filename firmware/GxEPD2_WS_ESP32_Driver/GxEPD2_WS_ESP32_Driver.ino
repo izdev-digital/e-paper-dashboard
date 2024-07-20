4880dd0e19ced7683b99c948706eea7f35e60190
ihor.zadorozhnyi <izdev.digital@gmail.com>
2024-07-20 10:28:14 +0200

    remove redundant code

--- firmware/GxEPD2_WS_ESP32_Driver/GxEPD2_WS_ESP32_Driver.ino
+++ firmware/GxEPD2_WS_ESP32_Driver/GxEPD2_WS_ESP32_Driver.ino
@@ -30,17 +30,9 @@
 // The logical configuration would be to use the e-paper connection on HSPI with re-mapped pins, and use VSPI for other SPI clients.
 // VSPI with standard VSPI pins is used by the global SPI instance of the Arduino IDE ESP32 package.
 
-// uncomment next line to use HSPI for EPD (and VSPI for SD), e.g. with Waveshare ESP32 Driver Board
-#define USE_HSPI_FOR_EPD
-
 // base class GxEPD2_GFX can be used to pass references or pointers to the display instance as parameter, uses ~1.2k more code
 // enable or disable GxEPD2_GFX base class
 #define ENABLE_GxEPD2_GFX 0
-
-// uncomment next line to use class GFX of library GFX_Root instead of Adafruit_GFX
-//#include <GFX.h>
-// Note: if you use this with ENABLE_GxEPD2_GFX 1:
-//       uncomment it in GxEPD2_GFX.h too, or add #include <GFX.h> before any #include <GxEPD2_GFX.h>
 
 // #include <GxEPD2_BW.h>
 #include <GxEPD2_3C.h>
@@ -50,7 +42,6 @@
 #define GxEPD2_DISPLAY_CLASS GxEPD2_3C
 #define GxEPD2_DRIVER_CLASS GxEPD2_750c_Z08 // GDEW075Z08  800x480, EK79655 (GD7965), (WFT0583CZ61)
 
-// somehow there should be an easier way to do this
 #define GxEPD2_BW_IS_GxEPD2_BW true
 #define GxEPD2_3C_IS_GxEPD2_3C true
 #define GxEPD2_7C_IS_GxEPD2_7C true
@@ -62,7 +53,7 @@
 #define IS_GxEPD2_1248(x) IS_GxEPD(GxEPD2_1248_IS_, x)
 
 #if defined(ESP32)
-#define MAX_DISPLAY_BUFFER_SIZE 65536ul // e.g.
+#define MAX_DISPLAY_BUFFER_SIZE 65536ul
 #if IS_GxEPD2_BW(GxEPD2_DISPLAY_CLASS)
 #define MAX_HEIGHT(EPD) (EPD::HEIGHT <= MAX_DISPLAY_BUFFER_SIZE / (EPD::WIDTH / 8) ? EPD::HEIGHT : MAX_DISPLAY_BUFFER_SIZE / (EPD::WIDTH / 8))
 #elif IS_GxEPD2_3C(GxEPD2_DISPLAY_CLASS)
@@ -73,37 +64,19 @@
 GxEPD2_DISPLAY_CLASS<GxEPD2_DRIVER_CLASS, MAX_HEIGHT(GxEPD2_DRIVER_CLASS)> display(GxEPD2_DRIVER_CLASS(/*CS=*/ 15, /*DC=*/ 27, /*RST=*/ 26, /*BUSY=*/ 25));
 #endif
 
-// comment out unused bitmaps to reduce code space used
 #include "bitmaps/Bitmaps200x200.h" // 1.54" b/w
-#include "bitmaps/Bitmaps104x212.h" // 2.13" b/w flexible GDEW0213I5F
-#include "bitmaps/Bitmaps128x250.h" // 2.13" b/w
-#include "bitmaps/Bitmaps128x296.h" // 2.9"  b/w
-#include "bitmaps/Bitmaps176x264.h" // 2.7"  b/w
-#include "bitmaps/Bitmaps400x300.h" // 4.2"  b/w
-#include "bitmaps/Bitmaps640x384.h" // 7.5"  b/w
-// 3-color
-#include "bitmaps/Bitmaps3c200x200.h" // 1.54" b/w/r
-#include "bitmaps/Bitmaps3c104x212.h" // 2.13" b/w/r
-#include "bitmaps/Bitmaps3c128x296.h" // 2.9"  b/w/r
-#include "bitmaps/Bitmaps3c176x264.h" // 2.7"  b/w/r
-#include "bitmaps/Bitmaps3c400x300.h" // 4.2"  b/w/r
 
-#if defined(ESP32) && defined(USE_HSPI_FOR_EPD)
 SPIClass hspi(HSPI);
-#endif
 
 void setup()
 {
   Serial.begin(115200);
-  // *** special handling for Waveshare ESP32 Driver board *** //
-  // ********************************************************* //
-#if defined(ESP32) && defined(USE_HSPI_FOR_EPD)
+  
   hspi.begin(13, 12, 14, 15); // remap hspi for EPD (swap pins)
+  
   display.epd2.selectSPI(hspi, SPISettings(4000000, MSBFIRST, SPI_MODE0));
-#endif
-  // *** end of special handling for Waveshare ESP32 Driver board *** //
-  // **************************************************************** //
   display.init(115200);
+  
   drawBitmaps();
   display.powerOff();
 }
@@ -121,22 +94,11 @@ void drawBitmaps()
 
 void drawBitmaps200x200()
 {
-#if defined(__AVR)
-  const unsigned char* bitmaps[] =
-  {
-    logo200x200, first200x200 //, second200x200, third200x200, fourth200x200, fifth200x200, sixth200x200, senventh200x200, eighth200x200
-  };
-#elif defined(_BOARD_GENERIC_STM32F103C_H_)
-  const unsigned char* bitmaps[] =
-  {
-    logo200x200, first200x200, second200x200, third200x200, fourth200x200, fifth200x200 //, sixth200x200, senventh200x200, eighth200x200
-  };
-#else
   const unsigned char* bitmaps[] =
   {
     logo200x200, first200x200, second200x200, third200x200, fourth200x200, fifth200x200, sixth200x200, senventh200x200, eighth200x200
   };
-#endif
+
   if ((display.epd2.panel == GxEPD2::GDEP015OC1) || (display.epd2.panel == GxEPD2::GDEH0154D67))
   {
     bool m = display.mirror(true);
