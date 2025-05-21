c582070a8a1d5643777a58d763808209022b7720
ihor.zadorozhnyi <izdev.digital@gmail.com>
2025-05-21 23:32:34 +0200

    added option to select format

--- software/EPaperDashboard/Controllers/RenderToImageController.cs
+++ software/EPaperDashboard/Controllers/RenderToImageController.cs
@@ -5,6 +5,10 @@ using System.ComponentModel.DataAnnotations;
 using EPaperDashboard.Utilities;
 using CSharpFunctionalExtensions;
 using EPaperDashboard.Models.Rendering;
+using SixLabors.ImageSharp.Formats.Bmp;
+using SixLabors.ImageSharp.Formats;
+using SixLabors.ImageSharp.Formats.Jpeg;
+using SixLabors.ImageSharp.Formats.Png;
 
 namespace EPaperDashboard.Controllers;
 
@@ -16,7 +20,7 @@ public class RenderToImageController(IPageToImageRenderingService renderingServi
 
 	[HttpGet]
 	[Route("binary")]
-	public async Task<IActionResult> GetAsBinary([Required][FromQuery] Size imageSize) => 
+	public async Task<IActionResult> GetAsBinary([Required][FromQuery] Size imageSize) =>
 		await RenderPage(
 			imageSize,
 			image => image
@@ -27,22 +31,28 @@ public class RenderToImageController(IPageToImageRenderingService renderingServi
 
 	[HttpGet]
 	[Route("converted")]
-	public async Task<IActionResult> GetAsConvertedsImage([Required][FromQuery] Size imageSize) =>
-		await RenderPage(
+	public async Task<IActionResult> GetAsConvertedsImage([Required][FromQuery] Size imageSize, [Required][FromQuery] string format)
+	{
+		var (contentType, encoder) = GetEncoder(format);
+		return await RenderPage(
 			imageSize,
 			image => image
 				.Quantize(Palettes.RedBlackWhite),
-			async (image, outStream) => await image.SaveJpegAsync(outStream),
-			"image/jpg");
+			async (image, outStream) => await image.SaveAsync(outStream, encoder),
+			contentType);
+	}
 
 	[HttpGet]
 	[Route("original")]
-	public async Task<IActionResult> GetAsImage([Required][FromQuery] Size imageSize) => 
-		await RenderPage(
+	public async Task<IActionResult> GetAsImage([Required][FromQuery] Size imageSize, [Required][FromQuery] string format)
+	{
+		var (contentType, encoder) = GetEncoder(format);
+		return await RenderPage(
 			imageSize,
 			image => image,
-			async (image, outStream) => await image.SaveJpegAsync(outStream),
-			"image/jpg");
+			async (image, outStream) => await image.SaveAsync(outStream, encoder),
+			contentType);
+	}
 
 	private async Task<IActionResult> RenderPage(Size imageSize, Func<IImage, IImage> convert, Func<IImage, MemoryStream, Task> serialize, string contentType) =>
 		await _renderingService
@@ -57,4 +67,12 @@ public class RenderToImageController(IPageToImageRenderingService renderingServi
 					return (IActionResult)File(outStream, contentType);
 				},
 				onFailure: error => Task.FromResult<IActionResult>(Problem(error, statusCode: StatusCodes.Status400BadRequest)));
+
+	private (string contentType, IImageEncoder encoder) GetEncoder(string format) => format switch
+	{
+		"jpeg" => ("image/jpeg", new JpegEncoder()),
+		"bmp" => ("image/bmp", new BmpEncoder()),
+		"png" => ("image/png", new PngEncoder()),
+		_ => throw new NotSupportedException($"Format is not supported: {format}")
+	};
 }
