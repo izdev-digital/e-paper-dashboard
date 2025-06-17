b23c3db56c02152a0ec3a0c3b618d57528788006
ihor.zadorozhnyi <izdev.digital@gmail.com>
2025-06-17 03:15:14 +0200

    adjust pages content

--- software/EPaperDashboard/Utilities/Constants.cs
+++ software/EPaperDashboard/Utilities/Constants.cs
@@ -2,6 +2,7 @@
 
 public static class Constants
 {
+    public const string AppName = "E-Paper Dashboard";
 	public const string DashboardHttpClientName = nameof(DashboardHttpClientName);
 
 	public const string HassHttpClientName = nameof(HassHttpClientName);
