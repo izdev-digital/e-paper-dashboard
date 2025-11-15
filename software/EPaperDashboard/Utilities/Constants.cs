8f5ba76f0126ffaae0be1d8c2abea25fc6e2cda9
ihor.zadorozhnyi <izdev.digital@gmail.com>
2025-11-15 13:10:44 +0100

    get back removed constants

--- software/EPaperDashboard/Utilities/Constants.cs
+++ software/EPaperDashboard/Utilities/Constants.cs
@@ -3,8 +3,11 @@
 public static class Constants
 {
     public const string AppName = "E-Paper Dashboard";
-	public const string DashboardHttpClientName = nameof(DashboardHttpClientName);
 
-	public const string HassHttpClientName = nameof(HassHttpClientName);
     public const string CompanyName = "izdev.digital";
+
+    public const string DashboardHttpClientName = nameof(DashboardHttpClientName);
+
+    public const string HassHttpClientName = nameof(HassHttpClientName);
+    
 }
