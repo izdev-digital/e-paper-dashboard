7f41445d0c8dfddfc459096257a8637457a9cd06
ihor.zadorozhnyi <izdev.digital@gmail.com>
2025-11-15 15:10:15 +0100

    display version in the software

--- software/EPaperDashboard/Utilities/Constants.cs
+++ software/EPaperDashboard/Utilities/Constants.cs
@@ -1,4 +1,5 @@
-﻿namespace EPaperDashboard.Utilities;
+﻿
+namespace EPaperDashboard.Utilities;
 
 public static class Constants
 {
@@ -6,8 +7,18 @@ public static class Constants
 
     public const string CompanyName = "izdev.digital";
 
+    public static string AppVersion { get; } = GetAppVersion();
+
+    private static string GetAppVersion()
+    {
+        var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+        var version = assembly.GetName().Version;
+        
+        return version?.ToString() ?? "0.0.0";
+    }
+
     public const string DashboardHttpClientName = nameof(DashboardHttpClientName);
 
     public const string HassHttpClientName = nameof(HassHttpClientName);
-    
+
 }
