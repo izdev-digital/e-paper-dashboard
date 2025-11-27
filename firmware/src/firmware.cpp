78d79c8eafeec8a9cad17bba82ec4ce5164a3b91
ihor.zadorozhnyi <izdev.digital@gmail.com>
2025-11-27 23:08:30 +0100

    added captivity portal

--- firmware/src/firmware.cpp
+++ firmware/src/firmware.cpp
@@ -3,6 +3,7 @@
 #include <optional>
 #include <WiFi.h>
 #include <WebServer.h>
+#include <DNSServer.h>
 #include <driver/rtc_io.h>
 #include "version.h"
 
@@ -11,7 +12,7 @@
 #include <GxEPD2_3C.h>
 
 #define GxEPD2_DISPLAY_CLASS GxEPD2_3C
-#define GxEPD2_DRIVER_CLASS GxEPD2_750c_Z08  // GDEW075Z08  800x480, EK79655 (GD7965), (WFT0583CZ61)
+#define GxEPD2_DRIVER_CLASS GxEPD2_750c_Z08 // GDEW075Z08  800x480, EK79655 (GD7965), (WFT0583CZ61)
 
 #define GxEPD2_3C_IS_GxEPD2_3C true
 #define IS_GxEPD(c, x) (c##x)
@@ -47,12 +48,13 @@ static const uint16_t displayHeight = 480;
 static const uint16_t frameWidth = displayWidth;
 static const uint16_t frameHeight = 32;
 static const uint16_t frameBytes = frameWidth * frameHeight / 8;
-static uint8_t epd_bitmap_BW[frameBytes] = { 0 };
-static uint8_t epd_bitmap_RW[frameBytes] = { 0 };
+static uint8_t epd_bitmap_BW[frameBytes] = {0};
+static uint8_t epd_bitmap_RW[frameBytes] = {0};
 
 SPIClass hspi(HSPI);
 
-struct Configuration {
+struct Configuration
+{
   String ssid;
   String password;
   String dashboardUrl;
@@ -60,7 +62,6 @@ struct Configuration {
   uint64_t dashboardRate;
   String dashboardApiKey;
 };
-
 
 void startDeepSleep(const Configuration &config);
 std::optional<Configuration> getConfiguration();
@@ -76,27 +77,31 @@ void fetchBinaryData(const Configuration &config);
 std::optional<uint64_t> fetchNextWaitSeconds(const Configuration &config);
 bool trySendGetRequest(WiFiClient &client, const String &url, const Configuration &config);
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
-  hspi.begin(13, 12, 14, 15);  // remap hspi for EPD (swap pins)
+  hspi.begin(13, 12, 14, 15); // remap hspi for EPD (swap pins)
   display.epd2.selectSPI(hspi, SPISettings(4000000, MSBFIRST, SPI_MODE0));
   display.init(115200);
 
   Serial.print("E-Paper Dashboard Firmware v");
   Serial.println(FIRMWARE_VERSION);
 
-  if (isResetRequested()) {
+  if (isResetRequested())
+  {
     Serial.println("Resetting device");
     resetDevice();
   }
 
   const auto configuration = getConfiguration();
-  if (!configuration.has_value()) {
+  if (!configuration.has_value())
+  {
     createConfiguration();
     return;
   }
 
-  if (connectToWiFi(configuration.value())) {
+  if (connectToWiFi(configuration.value()))
+  {
     fetchBinaryData(configuration.value());
   }
 
@@ -106,21 +111,25 @@ void setup() {
   startDeepSleep(configuration.value());
 }
 
-void loop() {
+void loop()
+{
   // This function should not be reached. Restarting in case this happened.
   ESP.restart();
 }
 
-void fetchBinaryData(const Configuration &config) {
+void fetchBinaryData(const Configuration &config)
+{
   Serial.println("Connecting to the remote server...");
 
   WiFiClient client;
-  if (!trySendGetRequest(client, "/api/render/binary?width=800&height=480", config)) {
+  if (!trySendGetRequest(client, "/api/render/binary?width=800&height=480", config))
+  {
     Serial.println("Failed to connect to the remote server...");
     return;
   }
 
-  if (!hasSuccessfulStatusCode(client)) {
+  if (!hasSuccessfulStatusCode(client))
+  {
     Serial.println("The request was not successful...");
     return;
   }
@@ -129,8 +138,10 @@ void fetchBinaryData(const Configuration &config) {
   int16_t x = 0;
   int16_t y = 0;
 
-  while ((client.connected() || client.available()) && y < displayHeight) {
-    for (int16_t pixelCount = 0; pixelCount < frameBytes && client.available(); pixelCount++) {
+  while ((client.connected() || client.available()) && y < displayHeight)
+  {
+    for (int16_t pixelCount = 0; pixelCount < frameBytes && client.available(); pixelCount++)
+    {
       uint8_t blackPixel = client.read();
       uint8_t redPixel = client.read();
       epd_bitmap_BW[pixelCount] = blackPixel;
@@ -146,44 +157,52 @@ void fetchBinaryData(const Configuration &config) {
   client.stop();
 }
 
-std::optional<uint64_t> fetchNextWaitSeconds(const Configuration &config) {
+std::optional<uint64_t> fetchNextWaitSeconds(const Configuration &config)
+{
   Serial.println("Connecting to the remote server...");
 
   WiFiClient client;
-  if (!trySendGetRequest(client, "/api/configuration/next-update-wait-seconds", config)) {
+  if (!trySendGetRequest(client, "/api/configuration/next-update-wait-seconds", config))
+  {
     return std::nullopt;
   }
 
-  if (!hasSuccessfulStatusCode(client)) {
+  if (!hasSuccessfulStatusCode(client))
+  {
     Serial.println("The request was not successful...");
     return std::nullopt;
   }
 
   Serial.println("Reading content...");
   String delayString{};
-  if (client.connected() || client.available()) {
+  if (client.connected() || client.available())
+  {
     delayString = client.readStringUntil('\n');
     Serial.println(delayString);
   }
 
   client.stop();
-  return delayString.length() > 0 
-    ? std::make_optional(strtoull(delayString.c_str(), nullptr, 10))
-    : std::nullopt;
+  return delayString.length() > 0
+             ? std::make_optional(strtoull(delayString.c_str(), nullptr, 10))
+             : std::nullopt;
 }
 
-bool hasSuccessfulStatusCode(WiFiClient &client) {
+bool hasSuccessfulStatusCode(WiFiClient &client)
+{
   Serial.println("Reading headers...");
   bool isStatusOk = false;
-  while (client.connected() || client.available()) {
+  while (client.connected() || client.available())
+  {
     String line = client.readStringUntil('\n');
     Serial.println(line);
 
-    if (!isStatusOk) {
+    if (!isStatusOk)
+    {
       isStatusOk = line.startsWith("HTTP/1.1 200 OK");
     }
 
-    if (line == "\r") {  // Headers end with an empty line
+    if (line == "\r")
+    { // Headers end with an empty line
       break;
     }
   }
@@ -191,8 +210,10 @@ bool hasSuccessfulStatusCode(WiFiClient &client) {
   return isStatusOk;
 }
 
-bool trySendGetRequest(WiFiClient &client, const String &url, const Configuration &config) {
-  if (!client.connect(config.dashboardUrl.c_str(), config.dashboardPort)) {
+bool trySendGetRequest(WiFiClient &client, const String &url, const Configuration &config)
+{
+  if (!client.connect(config.dashboardUrl.c_str(), config.dashboardPort))
+  {
     Serial.println("Failed to connect to the remote server...");
     return false;
   }
@@ -206,11 +227,12 @@ bool trySendGetRequest(WiFiClient &client, const String &url, const Configuratio
   client.print(config.dashboardUrl);
   client.print(":");
   client.println(config.dashboardPort);
-  client.println();  // This line sends the request
+  client.println(); // This line sends the request
   return true;
 }
 
-void startDeepSleep(const Configuration &config) {
+void startDeepSleep(const Configuration &config)
+{
   uint64_t waitSeconds = fetchNextWaitSeconds(config).value_or(config.dashboardRate);
   uint64_t waitMicroseconds = waitSeconds * SEC_TO_USEC_FACTOR;
   esp_sleep_enable_timer_wakeup(waitMicroseconds);
@@ -220,25 +242,26 @@ void startDeepSleep(const Configuration &config) {
   esp_deep_sleep_start();
 }
 
-std::optional<Configuration> getConfiguration() {
+std::optional<Configuration> getConfiguration()
+{
   Preferences preferences{};
   preferences.begin(CONFIGURATION_NAMESPACE, true);
   Configuration configuration{
-    preferences.getString(CONFIGURATION_SSID, ""),
-    preferences.getString(CONFIGURATION_PASSWORD, ""),
-    preferences.getString(CONFIGURATION_DASHBOARD_URL, ""),
-    preferences.getInt(CONFIGURATION_DASHBOARD_PORT, 80),
-    preferences.getULong64(CONFIGURATION_DASHBOARD_RATE, 60),
-    preferences.getString(CONFIGURATION_DASHBOARD_API_KEY, "")
-  };
+      preferences.getString(CONFIGURATION_SSID, ""),
+      preferences.getString(CONFIGURATION_PASSWORD, ""),
+      preferences.getString(CONFIGURATION_DASHBOARD_URL, ""),
+      preferences.getInt(CONFIGURATION_DASHBOARD_PORT, 80),
+      preferences.getULong64(CONFIGURATION_DASHBOARD_RATE, 60),
+      preferences.getString(CONFIGURATION_DASHBOARD_API_KEY, "")};
   preferences.end();
 
   return configuration.ssid.length() == 0 || configuration.dashboardUrl.length() == 0
-           ? std::nullopt
-           : std::make_optional(configuration);
+             ? std::nullopt
+             : std::make_optional(configuration);
 }
 
-void storeConfiguration(const Configuration &config) {
+void storeConfiguration(const Configuration &config)
+{
   Preferences preferences{};
   preferences.begin(CONFIGURATION_NAMESPACE, false);
   preferences.putString(CONFIGURATION_SSID, config.ssid);
@@ -250,17 +273,29 @@ void storeConfiguration(const Configuration &config) {
   preferences.end();
 }
 
-void clearConfiguration() {
+void clearConfiguration()
+{
   Preferences preferences{};
   preferences.begin(CONFIGURATION_NAMESPACE, false);
   preferences.clear();
   preferences.end();
 }
 
-void createConfiguration() {
+void createConfiguration()
+{
+  IPAddress apIP(192, 168, 4, 1);
+  IPAddress gateway(192, 168, 4, 1);
+  IPAddress subnet(255, 255, 255, 0);
+  WiFi.softAPConfig(apIP, gateway, subnet);
   WiFi.softAP("EPaperDashboard-AP");
+  apIP = WiFi.softAPIP();
   Serial.print("AP IP address: ");
-  Serial.println(WiFi.softAPIP());
+  Serial.println(apIP);
+
+  // DNS server setup: redirect all domains to ESP32 AP IP
+  const byte DNS_PORT = 53;
+  DNSServer dnsServer;
+  dnsServer.start(DNS_PORT, "*", apIP);
 
   WebServer server(80);
   const char *htmlForm = R"rawliteral(
@@ -354,11 +389,11 @@ void createConfiguration() {
 
     </html>
     )rawliteral";
-  server.on("/", [&server, htmlForm]() {
-    server.send(200, "text/html", htmlForm);
-  });
+  server.on("/", [&server, htmlForm]()
+            { server.send(200, "text/html", htmlForm); });
 
-  server.on("/submit", HTTP_POST, [&server, htmlForm]() {
+  server.on("/submit", HTTP_POST, [&server, htmlForm]()
+            {
     const String ssidParam{ "ssid" };
     const String passParam{ "password" };
     const String urlParam{ "dashboard_url" };
@@ -404,29 +439,49 @@ void createConfiguration() {
 
     server.send(200, "text/html", "Settings saved. Rebooting...");
     delay(1000);
-    ESP.restart();
+    ESP.restart(); });
+
+  auto redirectToRoot = [&server]() {
+    server.sendHeader("Location", "/", true);
+    server.send(302, "text/plain", "Redirecting to setup page...");
+  };
+
+  server.on("/generate_204", redirectToRoot);
+  server.on("/hotspot-detect.html", redirectToRoot);
+  server.on("/ncsi.txt", redirectToRoot);
+  server.onNotFound([&server, htmlForm, &redirectToRoot]() {
+    if (server.uri() == "/submit") {
+      server.send(404, "text/plain", "Not found");
+      return;
+    }
+    redirectToRoot();
   });
 
   server.begin();
   Serial.println("HTTP server started");
 
-  while (true) {
+  while (true)
+  {
+    dnsServer.processNextRequest();
     server.handleClient();
     delay(2);
   }
 }
 
-bool connectToWiFi(const Configuration &config) {
+bool connectToWiFi(const Configuration &config)
+{
   Serial.println("Found stored configuration!");
   Serial.println("Connecting to WiFi");
   const auto maxConnectionTestRetries = 20;
   auto connectionTestRetry = 0;
   WiFi.begin(config.ssid.c_str(), config.password.c_str());
-  while (WiFi.status() != WL_CONNECTED && connectionTestRetry < maxConnectionTestRetries) {
+  while (WiFi.status() != WL_CONNECTED && connectionTestRetry < maxConnectionTestRetries)
+  {
     ++connectionTestRetry;
     delay(500);
   }
-  if (WiFi.status() != WL_CONNECTED) {
+  if (WiFi.status() != WL_CONNECTED)
+  {
     return false;
   }
 
@@ -436,16 +491,20 @@ bool connectToWiFi(const Configuration &config) {
   return true;
 }
 
-bool isResetRequested() {
+bool isResetRequested()
+{
   pinMode(RESET_WAKEUP_PIN, INPUT_PULLDOWN);
-  if (digitalRead(RESET_WAKEUP_PIN) != HIGH) {
+  if (digitalRead(RESET_WAKEUP_PIN) != HIGH)
+  {
     return false;
   }
 
   const unsigned long pressStartTime = millis();
   const unsigned long requiredWaitingTime = RESET_REQUEST_TIMEOUT * 1000;
-  while (digitalRead(RESET_WAKEUP_PIN) == HIGH) {
-    if (millis() - pressStartTime >= requiredWaitingTime) {
+  while (digitalRead(RESET_WAKEUP_PIN) == HIGH)
+  {
+    if (millis() - pressStartTime >= requiredWaitingTime)
+    {
       return true;
     }
   }
@@ -453,7 +512,8 @@ bool isResetRequested() {
   return false;
 }
 
-void resetDevice() {
+void resetDevice()
+{
   clearConfiguration();
   ESP.restart();
 }
