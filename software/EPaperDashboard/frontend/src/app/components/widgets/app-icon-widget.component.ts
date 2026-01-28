4e564190a7a81196fa3b30b9b61ed5da8c5ac242
ihor.zadorozhnyi <izdev.digital@gmail.com>
2026-01-28 21:04:54 +0100

    fix scaling of the app icon widget

--- software/EPaperDashboard/frontend/src/app/components/widgets/app-icon-widget.component.ts
+++ software/EPaperDashboard/frontend/src/app/components/widgets/app-icon-widget.component.ts
@@ -1,17 +1,11 @@
 import { Component, Input } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { WidgetConfig, ColorScheme } from '../../models/types';
 
 @Component({
   selector: 'app-widget-app-icon',
   standalone: true,
-  imports: [CommonModule],
   styleUrls: ['./app-icon-widget.component.scss'],
-  template: `
-    <div class="app-icon-widget">
-      <img [src]="asAppIconConfig(widget.config).iconUrl || '/icon.svg'" [style.width.px]="asAppIconConfig(widget.config).size || 48" [style.height.px]="asAppIconConfig(widget.config).size || 48" alt="App Icon" />
-    </div>
-  `
+  template: `<img class="app-icon" [src]="asAppIconConfig(widget.config).iconUrl || '/icon.svg'" alt="App Icon" />`
 })
 export class AppIconWidgetComponent {
   @Input() widget!: WidgetConfig;
